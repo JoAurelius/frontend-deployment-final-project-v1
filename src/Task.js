63767e22232fcbda0b60e0a8db83947b9ad890b8
JoAurelius <jonathan.arls@yahoo.com>
2022-12-20 11:52:06 +0700

    update firebase

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://gallery-app-2570570.web.app/" // TODO: replace this
 const studentName = "Jonathan Aurelius" // TODO: replace this
 const studentId = "FE2570570" // TODO: replace this
 
